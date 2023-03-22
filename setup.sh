cd40f3b804fddef7d08c749da5a4a92e56504110
chLi <565353780@qq.com>
2023-03-22 10:45:14 +0800

    add basic files and setup bash file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install torch torchvision torchaudio \
+  --index-url https://download.pytorch.org/whl/cu116
+
+pip install gradio albumentations opencv-contrib-python imageio imageio-ffmpeg \
+  pytorch-lightning omegaconf test-tube streamlit einops transformers webdataset \
+  kornia open_clip_torch invisible-watermark streamlit-drawable-canvas torchmetrics \
+  timm addict yapf prettytable safetensors basicsr
