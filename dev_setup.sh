da4fee03ee80f981f23c653d3b519a7f6139ba13
chLi <565353780@qq.com>
2023-03-23 22:25:40 +0800

    update setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -2,6 +2,8 @@ pip install torch torchvision torchaudio \
   --index-url https://download.pytorch.org/whl/cu116
 
 pip install gradio albumentations opencv-contrib-python imageio imageio-ffmpeg \
-  pytorch-lightning omegaconf test-tube streamlit einops transformers webdataset \
-  kornia open_clip_torch invisible-watermark streamlit-drawable-canvas torchmetrics \
+  omegaconf test-tube streamlit einops transformers webdataset kornia \
+  open_clip_torch invisible-watermark streamlit-drawable-canvas torchmetrics \
   timm addict yapf prettytable safetensors basicsr
+
+pip install pytorch-lightning==1.5.0
